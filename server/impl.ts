655c31c141be2dfc52dc57002545ae55d2851e15
Justin Young <justin_dean_young@yahoo.com>
2022-07-03 12:59:26 -0400

    updates

--- server/impl.ts
+++ server/impl.ts
@@ -1,29 +1,30 @@
-import { Methods, Context } from "./.hathora/methods";
-import { Response } from "../api/base";
-import {
-  Vector,
-  Ball,
-  Player,
-  PlayerState,
-  UserId,
-  IInitializeRequest,
-  IUpdatePlayerPositionRequest,
-} from "../api/types";
+import { Methods, Context } from './.hathora/methods';
+import { Response } from '../api/base';
+import { Vector, Ball, Player, PlayerState, UserId, IInitializeRequest, IUpdatePlayerPositionRequest, IJoinGameRequest, IStartGameRequest } from '../api/types';
 
 type InternalState = PlayerState;
 
 export class Impl implements Methods<InternalState> {
-  initialize(ctx: Context, request: IInitializeRequest): InternalState {
-    return {
-      Players: [],
-      Balls: [],
-    };
-  }
-  updatePlayerPosition(state: InternalState, userId: UserId, ctx: Context, request: IUpdatePlayerPositionRequest): Response {
-    return Response.error("Not implemented");
-  }
-  getUserState(state: InternalState, userId: UserId): PlayerState {
-    return state;
-  }
-  onTick(state: InternalState, ctx: Context, timeDelta: number): void {}
+    initialize(ctx: Context, request: IInitializeRequest): InternalState {
+        return {
+            Players: [],
+            Balls: [],
+        };
+    }
+
+    startGame(state: PlayerState, userId: string, ctx: Context, request: IStartGameRequest): Response {
+        throw new Error('Method not implemented.');
+    }
+
+    joinGame(state: PlayerState, userId: string, ctx: Context, request: IJoinGameRequest): Response {
+        throw new Error('Method not implemented.');
+    }
+
+    updatePlayerPosition(state: InternalState, userId: UserId, ctx: Context, request: IUpdatePlayerPositionRequest): Response {
+        return Response.error('Not implemented');
+    }
+    getUserState(state: InternalState, userId: UserId): PlayerState {
+        return state;
+    }
+    onTick(state: InternalState, ctx: Context, timeDelta: number): void {}
 }
