463349533c7d77bf4b0552720ffb41e9af345941
Justin Young <justin_dean_young@yahoo.com>
2022-07-04 14:35:56 -0400

    updated UI client bindings

--- client/web/src/index.ts
+++ client/web/src/index.ts
@@ -1,2 +1,82 @@
 import './style.css';
-console.log(`Hello World`);
+import { UI, UIView } from 'peasy-ui';
+import { HathoraClient, HathoraConnection, UpdateArgs } from '../../.hathora/client';
+import { AnonymousUserData } from '../../../api/base';
+
+const myApp = document.getElementById('myApp');
+let intervalID: NodeJS.Timer;
+
+const client = new HathoraClient();
+let token: string;
+let user: AnonymousUserData;
+let myConnection: HathoraConnection;
+let updateState = (update: UpdateArgs) => {};
+
+//Create UI String Template
+const template = `
+        <div>
+          <div class="instructions">Pong \${title} \${username}</div>
+          
+          <div class="flex small_width">
+            <button id="btnLogin" class="button" \${click@=>login}>Login</button>
+          </div>
+
+          <div class="flex spacedEqual large_width">
+            <button id="btnCreateGame" class="button" \${click@=>create}>Create Game</button>
+            <button id="btnConnectGame" class="button" \${click@=>connect}>Connect Game</button>
+            <label for="gameJoinID">Game ID</label>
+            <input id="gameJoinID" type="text" \${value <=> gameID}></input>
+          </div>
+
+          <div class="flex startLeft large_width">
+            <button id="btnJoinGame" class="button" \${click@=>join}>Join Game</button>
+            <button id="btnStartGame"  class="button" \${click@=>start}>Start Game</button>
+          </div>
+          <div class="instructions">Up/Down arrows move paddle, spacebar launches ball</div>
+          <div id='playArea' class="gameArea"></div>
+          
+        </div>
+      `;
+
+const model = {
+    login: async (event, model) => {
+        if (sessionStorage.getItem('token') === null) {
+            sessionStorage.setItem('token', await client.loginAnonymous());
+        }
+        token = sessionStorage.getItem('token');
+        user = HathoraClient.getUserFromToken(token);
+        model.username = `-> User Name: ${user.name}`;
+    },
+    create: async (event, model) => {
+        model.gameID = await client.create(token, {});
+        model.title = `-> Game ID: ${model.gameID}`;
+        history.pushState({}, '', `/${model.gameID}`);
+        myConnection = await client.connect(token, model.gameID);
+        myConnection.onUpdate(updateState);
+        myConnection.onError(console.error);
+        myConnection.joinGame({});
+    },
+    connect: async (event, model) => {
+        myConnection = await client.connect(token, model.gameID);
+        model.title = `-> Game ID: ${model.gameID}`;
+        myConnection.onUpdate(updateState);
+        myConnection.onError(console.error);
+        myConnection.joinGame({});
+    },
+    join: (event, model) => {
+        myConnection.joinGame({});
+    },
+    start: (event, model) => {
+        myConnection.startGame({});
+    },
+    title: '',
+    gameID: '',
+    username: '',
+};
+
+let myUI: UIView;
+myUI = UI.create(myApp, template, model);
+
+intervalID = setInterval(() => {
+    UI.update();
+}, 1000 / 60);
--- client/web/webpack.config.js
+++ client/web/webpack.config.js
@@ -1,41 +1,43 @@
-const path = require('path') 
-const mode = process.env.NODE_ENV == "production" ? "production" : "development" 
-const HtmlWebpackPlugin = require('html-webpack-plugin')
- 
+const path = require('path');
+const mode = process.env.NODE_ENV == 'production' ? 'production' : 'development';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
-  entry: {  
-    index: path.resolve(__dirname, './src/index.ts') 
-  }, 
-  output: {
-      path: path.resolve(__dirname, './build'), 
-      filename: '[name].bundle.js' 
-    }, 
-  mode: mode,
-  module: { 
-    rules: [ 
-    { 
-        test: /\.css$/, 
-        use: ['style-loader', 'css-loader'] 
-    }, 
-    {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,  
-        type: 'asset/resource', 
+    entry: {
+        index: path.resolve(__dirname, './src/index.ts'),
     },
-    {
-        test: /\.tsx?$/,  
-        use: 'ts-loader',  
-        exclude: /node_modules/,  
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].bundle.js',
     },
-    ], 
-    }, 
+    mode: mode,
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
     devtool: 'inline-source-map',
-    resolve: { 
-    extensions: ['.tsx', '.ts', '.js'], 
-    }, 
-  devtool: 'inline-source-map',
-  plugins: [   
-     new HtmlWebpackPlugin({   
-     template: path.resolve(__dirname, "./src/template.html")   
-  })   
-  ],  
-}  
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/template.html'),
+        }),
+        new webpack.EnvironmentPlugin({ COORDINATOR_HOST: 'coordinator.hathora.dev', MATCHMAKER_HOST: 'matchmaker.hathora.com' }),
+    ],
+};
